3867ac191ad0a6df8705df36c8be912041b491cb
d0522587879-ai <d0522587879@gmail.com>
2026-04-14 22:44:52 +0700

    Create TuViApp.jsx with initial React component

---
+++ TuViApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TuViApp = () => {
+    return (
+        <div>
+            <h1>Welcome to the Tu Vi Application</h1>
+            {/* Add your application logic here */}
+        </div>
+    );
+};
+
+export default TuViApp;
